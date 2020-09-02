3f831d375ceb863115701fcbab56e2eb7e58268e
Fateh <fateh137@gmail.com>
2020-09-02 22:14:52 +0100

    day03

---
+++ 101-HTML-CSS/day03/handleData.php
@@ -0,0 +1,23 @@
+<?php
+/* 
+    receive data
+    handle data
+
+*/
+//var_dump($_GET['firstName']);
+//var_dump($_GET['lastName']);
+//var_dump($_GET);
+//print_r($_GET);
+
+
+
+$firstName = $_GET['firstName'];
+$lastName = $_GET['lastName'];
+
+
+
+
+echo "Welcome Mr  " . $firstName . " " . $lastName ;
+
+
+?>
