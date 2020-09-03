2587b9891c85c0e7f72dc382391d24bab3f58465
malekbenz <malekbenz@gmail.com>
2020-09-03 11:43:43 +0100

    Structure

---
+++ 201-PHP-basic/Control Structures/if.php
@@ -0,0 +1,14 @@
+<?php
+$a = 10;
+$b = 15;
+if ($a > $b) {
+    echo "a is bigger than b";
+    $b = $a;
+}
+?>
+
+<?php if (true) : ?>
+    html code to run if condition is true
+<?php else : ?>
+    html code to run if condition is false
+<?php endif ?>
